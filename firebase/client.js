b39185b56d0c02aa0785817d8efcb601e2e7ef59
rai <rai@apurata.com>
2021-06-28 19:53:11 -0500

    add firebase

---
+++ firebase/client.js
@@ -0,0 +1,35 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBj3QlPNylWzKo0_-b3YPgNIRyHABp-U6Y",
+    authDomain: "store-f05ad.firebaseapp.com",
+    projectId: "store-f05ad",
+    storageBucket: "store-f05ad.appspot.com",
+    messagingSenderId: "628434065949",
+    appId: "1:628434065949:web:5fa3702842a99729c144f4",
+    measurementId: "G-JRBYE8N4J3"
+};
+
+!firebase.apps.length && firebase.initializeApp(firebaseConfig);
+
+const mapUserFromFirebaseAuth = (user) => {
+    return {
+        name: user.displayName,
+        email: user.email,
+        avatar: user.photoURL
+    };
+};
+
+export const onAuthStateChange = (onChange) => {
+    return firebase.auth().onAuthStateChanged((user) => {
+        const userNormalice = user ? mapUserFromFirebaseAuth(user) : null
+        onChange(userNormalice);
+    });
+};
+
+export const loginWithGoogle = () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    return firebase.auth().signInWithPopup(googleProvider);
+};
