c2b8b6037982a6748fee7752287f8d2ac76f930e
rai <rai@apurata.com>
2021-07-13 21:32:14 -0500

    change api root

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "https://store-eight-azure.vercel.app/*"
+            }
+        ];
+    }
+};
--- services/ApiRoot.js
+++ services/ApiRoot.js
@@ -3,6 +3,6 @@ export const getApiRoot = () => {
     if (process.env.NODE_ENV === 'development') {
         return "http://localhost:3000/api"
     } else {
-        return "https://next-store-mf6ydczin-alexrai98.vercel.app/api"
+        return "https://store-eight-azure.vercel.app/api"
     }
 }
