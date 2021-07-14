4f9ecd906e0e0c19b4bc2de35e8c0a70816129d3
rai <rai@apurata.com>
2021-07-13 20:18:31 -0500

    expose mongo credentials4

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
 
 const connection = {};
+const MONGO_URI = "mongodb+srv://rai_98:XJD9AJv3u4a3yIAT@webstore.nu2qu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// const MONGO_URI = process.env.MONGODB_URI
 
 export async function dbConnect() {
     if (connection.isConnected) {
         return;
     }
 
-    const db = await mongoose.connect(process.env.MONGODB_URI, {
+    const db = await mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
